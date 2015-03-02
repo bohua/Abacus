a2e30e355476bcb14d5ea4f269cf785f7dd46cdd
bohua <fantasybohua@gmail.com>
2015-03-02 23:21:31 +0100

    add editable table

--- Abacus.WebUI/Scripts/app.js
+++ Abacus.WebUI/Scripts/app.js
@@ -1,4 +1,12 @@
-﻿var Abacus = angular.module('Abacus', ['ngRoute', 'sales', '$api']);
+﻿var Abacus = angular.module('Abacus', [
+    //ngModels
+    'ngRoute',
+    //Services
+    '$api',
+    //Partials
+    'sales',
+    'product'
+    ]);
 
 Abacus.controller('abacusController', ['$api', function () {
     //$api.init("http://localhost:40001");
---
+++ Abacus.WebUI/Scripts/partials/basic/product/product-controller.js
@@ -0,0 +1,49 @@
+﻿angular.module('product', ['$api'])
+    .config(['$routeProvider', function ($routeProvider) {
+        $routeProvider.
+            when('/ProductView', {
+                templateUrl: 'Scripts/partials/basic/product/product-view.tpl.html',
+                controller: 'productController'
+            });
+    }])
+	.controller('productController', ['$scope', '$http', '$api', function ($scope, $http, $api) {
+	    $api.get("/api/Product")
+            .success(function (data) {
+                //$scope.Products = data;
+
+                var dto = $.map(data, function(n){
+                    return {
+                        Id: n.Id,
+                        ProductName: n.ProductName,
+                        ProductPrice: n.ProductPrice
+                    };
+                });
+
+                console.log(dto);
+
+                $('#product-list').bootstrapTable({
+                    sortName: 'ProductName',
+                    sortOrder: 'asc',
+                    columns: [{
+                        field: 'Id',
+                        title: '代码'
+                    }, {
+                        field: 'ProductName',
+                        title: '产品',
+                        sortable: true,
+                        editable: {
+                            type: 'text'
+                        }
+                    }, {
+                        field: 'ProductPrice',
+                        title: '价格',
+                        sortable: true,
+                        editable: {
+                            type: 'text'
+                        }
+                    }],
+                    data: dto
+                });
+            });
+
+	}]);
